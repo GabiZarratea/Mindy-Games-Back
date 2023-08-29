567a7b8bf0b2a4e2d06c1a21a6202d86f6ee0330
Donato Zollo <davinazarra@gmail.com>
2023-08-29 16:50:08 -0300

    functional login

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,16 @@ import validator from "../middlewares/validator.js";
 import accountExists from '../middlewares/accountExists.js'
 import createHash from '../middlewares/createHash.js'
 import read from "../controllers/users/read.js";
+import userLogin from "../schemas/user/userSigninSchema.js";
+import accountNotExist from '../middlewares/accountNotExist.js';
+import passwordIsOk from '../middlewares/passwordIsOk.js';
+import generateToken from '../middlewares/generateToken.js';
+import login from "../controllers/users/login.js";
 
 const user_router = Router();
 
 user_router.get('/', read)
 user_router.post('/register', validator(userRegister), accountExists, createHash, register)
+user_router.post('/login', validator(userLogin), accountNotExist, passwordIsOk, generateToken, login)
 
 export default user_router;
